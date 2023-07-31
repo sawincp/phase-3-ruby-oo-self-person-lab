a57b3a54d3a3efb143c31722b8fd6b762d5af6d5
Corey Sawin <sawincp@gmail.com>
2023-07-31 16:23:16 -0400

    commit tree c3dea7babcbc238a95b53fe769a8b6db4c2937ef

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,58 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account :happiness :hygiene
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness(num)
+        @happiness = 
+        if num > 10
+            10
+        elsif num <10
+            0
+        else
+            num
+        end
+    end
+    
+    def hygiene(num)
+        @hygiene = 
+        if num > 10
+            10
+        elsif num <10
+            0
+        else
+            num
+        end
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def get_paid(amount)
+      self.bank_account += amount
+      "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+end
